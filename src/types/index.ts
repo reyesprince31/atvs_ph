1ff530bdde49fedd93c02da93ffbcb2b63df1f2e
Prince Reyes <krayzerprince@gmail.com>
2023-12-09 14:47:44 +0800

    work with signup newUser to appwrite

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+export type INavLink = {
+  imgURL: string;
+  route: string;
+  label: string;
+};
+
+export type IUpdateUser = {
+  userId: string;
+  name: string;
+  bio: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+};
+
+export type IUser = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  bio: string;
+};
+
+export type INewUser = {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+};
