d0275fb941f15b283067bf576b1a73cb4752149b
Prince Reyes <krayzerprince@gmail.com>
2023-12-09 13:43:40 +0800

    work with SignupForm using ShadCN ui

--- src/_auth/Authlayout.tsx
+++ src/_auth/Authlayout.tsx
@@ -1,5 +1,20 @@
+import { Outlet, Navigate } from "react-router-dom";
+
 const Authlayout = () => {
-  return <div>Authlayout</div>;
+  const isAuth = false;
+  return (
+    <>
+      {isAuth ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex py-10 flex-1 flex-col items-center justify-center">
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Authlayout;
